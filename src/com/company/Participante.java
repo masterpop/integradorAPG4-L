6af39b1f5f952250cc19b2f4f77b3795087a1ea8
masterpop <isadsoria@hotmail.es>
2023-03-22 11:23:49 -0300

    Clase Participante ejercicio 2

---
+++ src/com/company/Participante.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Participante {
+
+    private String Nombre;
+
+    private int Puntuacion ;
+
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public int getPuntuacion() {
+        return Puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        Puntuacion = puntuacion;
+    }
+}
